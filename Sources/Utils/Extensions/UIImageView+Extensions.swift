5414e021d5d0872aa2e6799d35ad73d998ff449d
Damian Kołakowski <damian.k@opensooq.com>
2019-01-09 11:13:01 +0100

    Fix rendering of iCloud optimized photos.

--- Sources/Utils/Extensions/UIImageView+Extensions.swift
+++ Sources/Utils/Extensions/UIImageView+Extensions.swift
@@ -17,7 +17,7 @@ extension UIImageView {
     }
 
     let options = PHImageRequestOptions()
-
+    options.isNetworkAccessAllowed = true
     let id = PHImageManager.default().requestImage(for: asset, targetSize: frame.size,
                                                                   contentMode: .aspectFill, options: options)
     { [weak self] image, _ in
